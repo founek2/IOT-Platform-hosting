632851e81d5fbb0e57da3cb05934c6ef6d45e95a
Martin Skalický <skalicky.martas@seznam.cz>
2022-06-14 22:04:18 +0200

    Add .env

--- rabbitmq/docker-entrypoint.sh
+++ rabbitmq/docker-entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 if [ ! -f "/keys/server_key.pem" ]; then
-    cd / && git clone https://github.com/michaelklishin/tls-gen tls-gen && cd tls-gen/basic &&
-        make &&
-        cp ./result/ca_certificate.pem ./result/server_certificate.pem ./result/server_key.pem /keys/
+    cd /tls-gen/basic
+    make CN=mqtt
+    mv ./result/ca_certificate.pem /keys/
+    mv ./result/server_mqtt_certificate.pem /keys/server_certificate.pem
+    mv ./result/server_mqtt_key.pem /keys/server_key.pem
 fi
 
 chown rabbitmq:rabbitmq /keys/{ca_certificate.pem,server_certificate.pem,server_key.pem}
