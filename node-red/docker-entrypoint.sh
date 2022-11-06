30e474e73de714aa4c3d29b195743c7dfab207c3
Martin Skalický <skalicky.martas@seznam.cz>
2022-11-06 17:55:17 +0100

    Fix node-red

--- node-red/docker-entrypoint.sh
+++ node-red/docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-if [ ! -f "/node-red/data/settings.js" ]; then
-    cp settings.js user-authentication.js /node-red/data
-fi
-
 npm --no-update-notifier --no-fund start --cache /node-red/data/.npm -- --userDir /node-red/data --settings /node-red/settings.js
 # tail -f /dev/null
